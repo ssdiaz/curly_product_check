b1b44830a38d4b7becf29f6cb806bd988d89afdd
Samantha Diaz <samidiaz3@gmail.com>
2021-09-27 22:49:40 -0400

    enviroment.rb  commenting out unused code and require_relatives

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,6 +1,6 @@
 #require_relative './curly_product_check/version'
 require_relative "curly_product_check/version"
-require_relative "./curly_product_check/cli"
+require_relative "curly_product_check/cli"
 require_relative "./curly_product_check/category"
 require_relative "./curly_product_check/scraper"
 require_relative "./curly_product_check/brand"
@@ -11,7 +11,7 @@ require 'pry'
 require 'nokogiri'
 require 'open-uri' #to look outside local files and into web
 
-module CurlyProductCheck
-  class Error < StandardError; end
-  # Your code goes here...
-end
+# module CurlyProductCheck
+#   class Error < StandardError; end
+#   # Your code goes here...
+# end
